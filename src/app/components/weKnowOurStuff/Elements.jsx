2f6fd91e8dd7dd5c03def1e1777f4e396517b069
Abhishek Choudhary <itzabhishek5127@gmail.com>
2026-06-21 11:59:43 +0530

    water distortion inspired

--- src/app/components/weKnowOurStuff/Elements.jsx
+++ src/app/components/weKnowOurStuff/Elements.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Elements = () => {
   return (
-    <div className='w-full h-[40vh] bg-orange-600 flex justify-center items-center'>
+    <div className='w-full h-[50vh] bg-orange-600 flex justify-center items-center'>
       <div>
         <div className='text-6xl select-none'>
             We Know Our Stuff Better Than Anybody!!
