ef7c75958bf79ba77ebb3efc9a3c4e330da7d3a5
Abhishek Choudhary <itzabhishek5127@gmail.com>
2026-06-21 10:57:45 +0530

    initial multiEffect component

---
+++ src/app/components/weKnowOurStuff/Elements.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Elements = () => {
+  return (
+    <div className='w-full h-[40vh] bg-orange-600 flex justify-center items-center'>
+      <div>
+        <div className='text-6xl select-none'>
+            We Know Our Stuff Better Than Anybody!!
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Elements
